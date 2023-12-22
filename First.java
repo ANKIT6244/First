b7b64af999f11b4903d060371344324044ea2a3c
rishu2000 <kumarrishavshah2000@gmail.com>
2023-12-22 13:45:19 +0530

    Corrected println statement

--- First.java
+++ First.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class First {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the to find the number in decending order:");
+        System.out.println("Enter the number to print it in decending order:");
         int i = scan.nextInt();
-        System.out.println("The number is: ");
+        System.out.println("The number's decending order is: ");
         do {
             System.out.println(i);
             i--;
