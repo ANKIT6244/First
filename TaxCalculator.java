91fcbac8ce83e7481f0fcaad483f45782e5f33de
ANKIT6244 <ankitkumarshah5050@gmail.com>
2023-12-29 20:59:56 +0530

    improved some code

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class TaxCalculator {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("   Tax Calculator App    ");
         System.out.println("_________Welcome_________");
-        Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter the total Person Count:");
         int n = scan.nextInt();
+
         String[] arr = new String[n];
         Long[] arry = new Long[n];
+
         for (int i = 0; i < n; i++) {
-            System.out.println("Enter Name:");
-            String name = scan.nextLine();
-            arr[i] = name;
-            /*
-             * System.out.println("Enter the " + arr[i] + " Income:");
-             * Long income = scan.nextLong();
-             * arry[i] = income;
-             */
+            System.out.println("Enter Name " + (i + 1) + ":");
+            arr[i] = scan.nextLine();
+
+            System.out.println("Enter the " + arr[i] + " Income:");
+            arry[i] = scan.nextLong();
+
         }
         scan.close();
     }
