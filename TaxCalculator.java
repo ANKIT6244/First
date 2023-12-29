3674f30a851d0880a4f810eeae7836915ea6ef58
ANKIT6244 <ankitkumarshah5050@gmail.com>
2023-12-30 00:31:20 +0530

    Project Completed of Tax Calculator

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -5,10 +5,9 @@ public class TaxCalculator {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("   Tax Calculator App    ");
-        System.out.println("_________Welcome_________");
-
-        System.out.println("Enter the total Person Count:");
+        System.out.println("\n     Tax Calculator App    ");
+        System.out.println(" ---------WELCOME----------");
+        System.out.println("\nEnter total Person Count:");
         int n = scan.nextInt();
 
         String[] arr = new String[n];
@@ -23,8 +22,26 @@ public class TaxCalculator {
 
             System.out.println("Enter " + arr[i] + "'s Annual Income:");
             arry[i] = scan.nextLong();
+        }
+        System.out.println("\n Names with liable taxes");
+        System.out.println("-----------------------------");
+        System.out.println();
 
+        for (int i = 0; i < n; i++) {
+            calculateTax(arr[i], arry[i]);
         }
         scan.close();
+    }
+
+    public static void calculateTax(String name, Long income) {
+        double tax = 0;
+        if (income >= 300000) {
+            tax = income * 20 / 100;
+        } else if (income < 300000 && income >= 100000) {
+            tax = income * 10 / 100;
+        } else if (income < 100000) {
+            tax = 0;
+        }
+        System.out.println(name + ": ₹" + tax);
     }
 }
